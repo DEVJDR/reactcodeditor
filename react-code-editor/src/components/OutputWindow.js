37f206bc8025b300bc7d643ab5dca887c90a6202
ARUN <arunjdev26@gmail.com>
2025-07-14 07:28:09 +0530

    first

---
+++ react-code-editor/src/components/OutputWindow.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const OutputWindow = ({ outputDetails }) => {
+  const getOutput = () => {
+    let statusId = outputDetails?.status?.id;
+
+    if (statusId === 6) {
+      // compilation error
+      return (
+        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+          {atob(outputDetails?.compile_output)}
+        </pre>
+      );
+    } else if (statusId === 3) {
+      return (
+        <pre className="px-2 py-1 font-normal text-xs text-green-500">
+          {atob(outputDetails.stdout) !== null
+            ? `${atob(outputDetails.stdout)}`
+            : null}
+        </pre>
+      );
+    } else if (statusId === 5) {
+      return (
+        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+          {`Time Limit Exceeded`}
+        </pre>
+      );
+    } else {
+      return (
+        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+          {atob(outputDetails?.stderr)}
+        </pre>
+      );
+    }
+  };
+  return (
+    <>
+      <h1 className="text-sm font-semibold mb-1 text-gray-300">
+        Output
+      </h1>
+      <div className="w-full h-36 focus:outline-none w-full  rounded-md  px-4 py-2 hover:shadow transition duration-200 bg-[#1e1e1e] mt-2">
+        {outputDetails ? <>{getOutput()}</> : null}
+      </div>
+    </>
+  );
+};
+
+export default OutputWindow;
